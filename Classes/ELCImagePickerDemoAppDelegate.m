4b0ab3d948b44a964e5f773b765dd38a81e9d2bb
Forrest <apple.dev.sh@gmail.com>
2010-11-30 09:38:31 +0800

    first commit codes

---
+++ Classes/ELCImagePickerDemoAppDelegate.m
@@ -0,0 +1,96 @@
+//
+//  ELCImagePickerDemoAppDelegate.m
+//  ELCImagePickerDemo
+//
+//  Created by Collin Ruffenach on 9/9/10.
+//  Copyright 2010 ELC Technologies. All rights reserved.
+//
+
+#import "ELCImagePickerDemoAppDelegate.h"
+#import "ELCImagePickerDemoViewController.h"
+
+#import "FacebookAgent.h"
+
+
+@implementation ELCImagePickerDemoAppDelegate
+
+@synthesize window;
+@synthesize viewController;
+
+
+#pragma mark -
+#pragma mark Application lifecycle
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {    
+    
+    // Override point for customization after application launch.
+	//iShare for apple.dev.sh@gmail.com
+	[[FacebookAgent sharedAgent] initializeWithApiKey:@"651d945962b53425dded877ad2b7d4d9" 
+											ApiSecret:@"b5220a148c340dec5f86050bd4c6e520" 
+											 ApiProxy:nil];
+	
+
+    // Add the view controller's view to the window and display.
+    [window addSubview:viewController.view];
+    [window makeKeyAndVisible];
+
+    return YES;
+}
+
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    /*
+     Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+     Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+     */
+}
+
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    /*
+     Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+     If your application supports background execution, called instead of applicationWillTerminate: when the user quits.
+     */
+}
+
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    /*
+     Called as part of  transition from the background to the inactive state: here you can undo many of the changes made on entering the background.
+     */
+}
+
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    /*
+     Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+     */
+}
+
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    /*
+     Called when the application is about to terminate.
+     See also applicationDidEnterBackground:.
+     */
+}
+
+
+#pragma mark -
+#pragma mark Memory management
+
+- (void)applicationDidReceiveMemoryWarning:(UIApplication *)application {
+    /*
+     Free up as much memory as possible by purging cached data objects that can be recreated (or reloaded from disk) later.
+     */
+}
+
+
+- (void)dealloc {
+    [viewController release];
+    [window release];
+    [super dealloc];
+}
+
+
+@end
